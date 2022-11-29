4fea3d02ad0f3417e2f7d7465cb07b2b121b6c49
(김상민) <(samkim1130@naver.com)>
2022-11-29 09:13:40 +0900

    KoominGochi

--- KoominGochi.py
+++ KoominGochi.py
@@ -1,27 +1,28 @@
 import tkinter
-import tkinter.ttk as ttk
-from PyQt5.QtWidgets import*
+import tkinter.font
 from tkinter import*
 import random
 from Day import day
+from Ending import ending
 
-from Action import Action
+
 class KoominGochi():
 
     def __init__(self):
+        #윈도우 창 띄우기
         d = day()
         Act = Action()
-        #윈도우 창 띄우기
         self.window = Tk()
         self.window.title("KoominGochi")
         self.window.geometry("600x800")
         #window.configure(bg='white')
         self.window.resizable(width=False, height=False)
 
+
         #장소 버튼들
         button_school = tkinter.Button(self.window, text='학교',width=8,height=2, fg="blue")
         button_school.place(x=80, y=460)
-        button_club = tkinter.Button(self.window, text='동아리', width=6, height=2, fg="blue", state="normal" , command=Act.goClub())
+        button_club = tkinter.Button(self.window, text='동아리', width=6, height=2, fg="blue")
         button_club.place(x=90, y=515)
         button_lectureRoom = tkinter.Button(self.window, text='강의실', width=6, height=2, fg="blue")
         button_lectureRoom.place(x=90, y=560)
@@ -67,23 +68,28 @@ class KoominGochi():
         button_stress.place(x=0, y=222)
 
         #능력치 상태바[지능, 매력, 돈, 외로움, 스트레스]
-        bar_intellect = tkinter.Button(self.window, text=Act.int, width=6, bg='green')
+        bar_intellect = tkinter.Button(self.window, text='50', width=6, bg='green')
         bar_intellect.place(x=60, y=100)
-        bar_charm = tkinter.Button(self.window, text=Act.char, width=6, bg='green')
+        bar_charm = tkinter.Button(self.window, text='50', width=6, bg='green')
         bar_charm.place(x=60, y=130)
-        bar_money = tkinter.Button(self.window, text=Act.money , width=6, bg='green')
+        bar_money = tkinter.Button(self.window, text='10', width=6, bg='green')
         bar_money.place(x=60, y=160)
-        bar_lonely = tkinter.Button(self.window, text=Act.lon, width=6, bg='green')
+        bar_lonely = tkinter.Button(self.window, text='90', width=6, bg='green')
         bar_lonely.place(x=60, y=190)
-        bar_stress = tkinter.Button(self.window, text=Act.str, width=6, bg='green')
+        bar_stress = tkinter.Button(self.window, text='90', width=6, bg='green')
         bar_stress.place(x=60, y=220)
 
         #날짜
-        button_day = tkinter.Label(self.window, text=d.showday(),width=6)
+        button_day = tkinter.Label(self.window, text=d.nextday(),width=6)
         button_day.place(x=0, y=12)
         #날짜 상태바
         bar_day = tkinter.Button(self.window, text=d.daypercent(), width=10, bg='green')
         bar_day.place(x=60, y=10)
+
+        # 엔딩 버튼 생성
+        font = tkinter.font.Font(family='Consolas', size=100)
+        self.ending = tkinter.Label(self.window, text="Ending.calu()", fg="pink", font=font)
+        self.ending.pack(side='right')
 
         self.window.mainloop()
 
@@ -91,11 +97,6 @@ class KoominGochi():
         self.kookmin_image = tkinter.PhotoImage(file='kookmin1.png')
         self.label = tkinter.Label(self.window, image=self.kookmin_image)
         self.label.place(x=140, y=170)
-
-
-
-
-
 
 
 
